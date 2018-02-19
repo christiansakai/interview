177757df6fb64f91bff75e16212a35d6f78b5901
Christian Sakai <christianmsakai@gmail.com>
2018-02-19 12:56:07 -0500

    add docs to stack and queue

--- dataStructure/stackQueue/queue.ts
+++ dataStructure/stackQueue/queue.ts
@@ -1,29 +1,72 @@
-import DoublyLL from '../linkedList/doubly'
+import SinglyLL from '../linkedList/singly'
 
+/**
+ * Stack implementation using singly linked list
+ */
 class Queue<T> {
 
-  private _list: DoublyLL<T>
+  /** Linked list to hold data */
+  private _list: SinglyLL<T>
 
   constructor() {
-    this._list = new DoublyLL()
+    this._list = new SinglyLL()
   }
 
+  /**
+   * Add an element to the end of the queue.
+   * 
+   * Time = O(1)
+   * Space = O(1)
+   *
+   * @param element Element to be added
+   */
   enqueue(element: T) {
     this._list.append(element)
   }
 
+  /**
+   * Remove an element from the front of the queue.
+   *
+   * Time = O(1)
+   * Space = O(1)
+   *
+   * @return Element to be removed or null 
+   *         if the queue is empty.
+   */
   dequeue(): T | null {
     return this._list.removeFromHead()
   }
 
+  /**
+   * Look at the element at the front of the queue.
+   *
+   * Time = O(1)
+   * Space = -
+   *
+   * @return Element at the front of the queue or null 
+   *         if the queue is empty.
+   */
   first(): T | null {
     return this._list.head
   }
 
+  /**
+   * Get the size of the queue.
+   *
+   * Time = O(1)
+   * Space = -
+   *
+   * @return Size of the queue
+   */
   size(): number {
     return this._list.length
   }
 
+  /**
+   * Check whether the stack is empty or not.
+   *
+   * @return `true` if the stack is empty, `false` otherwise
+   */
   isEmpty(): boolean {
     return this._list.length === 0
   }
@@ -31,4 +74,3 @@ class Queue<T> {
 }
 
 export default Queue
-
--- dataStructure/stackQueue/stack.ts
+++ dataStructure/stackQueue/stack.ts
@@ -1,29 +1,72 @@
-import SinglyLL from '../linkedList/singly'
+import DoublyLL from '../linkedList/doubly'
 
+/**
+ * Stack implementation using doubly linked list
+ */
 class Stack<T> {
 
-  private _list: SinglyLL<T>
+  /** Linked list to hold data */
+  private _list: DoublyLL<T>
 
   constructor() {
-    this._list = new SinglyLL()
+    this._list = new DoublyLL()
   }
 
+  /**
+   * Add an element to the top of the stack.
+   * 
+   * Time = O(1)
+   * Space = O(1)
+   *
+   * @param element Element to be added
+   */
   push(element: T) {
     this._list.append(element)
   }
 
+  /**
+   * Remove an element from the top of the stack.
+   *
+   * Time = O(1)
+   * Space = O(1)
+   *
+   * @return Element to be removed or null 
+   *         if the stack is empty.
+   */
   pop(): T | null {
     return this._list.removeFromTail()
   }
 
+  /**
+   * Look at the element on top of the stack.
+   *
+   * Time = O(1)
+   * Space = -
+   *
+   * @return Element on top of the stack or null 
+   *         if the stack is empty.
+   */
   top(): T | null {
     return this._list.tail
   }
 
+  /**
+   * Get the size of the stack.
+   *
+   * Time = O(1)
+   * Space = -
+   *
+   * @return Size of the stack
+   */
   size(): number {
     return this._list.length
   }
 
+  /**
+   * Check whether the stack is empty or not.
+   *
+   * @return `true` if the stack is empty, `false` otherwise
+   */
   isEmpty(): boolean {
     return this._list.length === 0
   }
